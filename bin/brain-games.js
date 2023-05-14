0037103294534aad3f1ce48fb578cdae9dee2c7c
arty_Pt <gideonko10@gmail.com>
2023-05-14 20:12:45 +0300

    Brain Games All games and support functions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,4 @@ import whatsYourName from '../src/cli.js';
 console.log('Welcome to the Brain Games! ');
 
 const name = whatsYourName();
-console.log(`Hello, ${name}`);
+console.log(`Hello, ${name}!`);
--- src/gameloop.js
+++ src/gameloop.js
@@ -1,6 +1,6 @@
 export default (name, question, askQuestion) => {
   let isCorrect = false;
-
+  console.log(`Hello, ${name}!`);
   console.log(question);
 
   for (let i = 0; i < 3; i += 1) {

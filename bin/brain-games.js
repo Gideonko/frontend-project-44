e0c6cb8a345a678aa15f3933a436ffeea9ab7a9f
arty_Pt <gideonko10@gmail.com>
2023-05-14 20:02:31 +0300

    Brain Games All games and support functions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-import { whatsYourName } from '../src/cli.js';
+import whatsYourName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games! ');
 
-whatsYourName();
+const name = whatsYourName();
+console.log(`Hello, ${name}`);
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,3 @@
 import readLineSync from 'readline-sync';
 
-const whatsYourName = () => {
-  const name = readLineSync.question('May i have your name?');
-
-  console.log(`Hello, ${name}`);
-};
-
-// eslint-disable-next-line import/prefer-default-export
-export { whatsYourName };
+export default () => readLineSync.question('May i have your name? ');
